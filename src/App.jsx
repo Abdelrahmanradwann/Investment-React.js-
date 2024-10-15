83d5c3e5f694bb6376444e55410b3fb2e25be9ef
abdo <boodyahmed825@gmail.com>
2024-10-15 22:09:58 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header.jsx';
+import InputBoard from './components/InputBoard.jsx';
+function App() {
+  return (
+    <>
+      <Header />
+      <InputBoard />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/InputBoard.jsx
@@ -0,0 +1,27 @@
+
+export default function InputBoard() {
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label>INITIAL INVESTMENT</label>
+                    <input type="number" required/>
+                </p>
+                <p>
+                    <label>ANNUAL INVESTMENT</label>
+                    <input type="number" required/>
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label>EXPECTED RETURN</label>
+                    <input type="number" required/>
+                </p>
+                <p>
+                    <label>DURATION</label>
+                    <input type="number" required/>
+                </p>
+            </div>
+        </section>
+    )
+}
